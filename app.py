bd3ed17768cf418e6cdb6634bee8a692e0196116
JoaoOscar01 <joao.santos2904@hotmail.com>
2025-02-18 18:04:56 -0300

    Subindo pagina 1

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Criação da aplicação Flask
+app = Flask(__name__)
+
+# Definindo a rota
+@app.route('/')
+def home():
+    return '<h1>Olá, Mundo!</h1>'
+
+# Rodando o servidor
+if __name__ == '__main__':
+    app.run(debug=True)
