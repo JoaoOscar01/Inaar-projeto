fbda844655d3b709966679b6a42a39bf69b86b3d
JoaoOscar01 <joao.santos2904@hotmail.com>
2025-02-19 00:51:32 -0300

    Adicionando pasta templates + static e adicionando pagina para cadastrar comida

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from flask import Flask, render_template
 # Criação da aplicação Flask
 app = Flask(__name__)
 
-# Definindo a rota
+# Definindo a rota de telas
 @app.route('/')
 def home():
     return render_template("pagina1.html")
+
+
+@app.route("/cadastrar")
+def cadastro_comida():
+        return render_template("cadastrar_comida.html")
 
 # Rodando o servidor
 if __name__ == '__main__':
