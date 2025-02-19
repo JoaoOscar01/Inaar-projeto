07981f3a16c59d9c855f2c7388fa4e0e7221eb19
JoaoOscar01 <joao.santos2904@hotmail.com>
2025-02-18 23:14:51 -0300

    Adicinando pasta template e jogando a pagina 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Criação da aplicação Flask
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Definindo a rota
 @app.route('/')
 def home():
-    return '<h1>Iniciando projeto</h1>'
+    return render_template("pagina1.html")
 
 # Rodando o servidor
 if __name__ == '__main__':
