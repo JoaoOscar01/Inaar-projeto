40da0c0892d36490d921c0bdde69fcc78fbda970
LuanDurbano <luandurbano@gmail.com>
2025-02-19 15:34:18 -0300

    sua mensagem

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-sobre').addEventListener('click', function() {
+    document.getElementById('sobre-nos').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('btn-contato').addEventListener('click', function() {
+    document.getElementById('contato').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('btn-cardapio').addEventListener('click', function() {
+    document.getElementById('cardapio').scrollIntoView({ behavior: 'smooth' });
+});
